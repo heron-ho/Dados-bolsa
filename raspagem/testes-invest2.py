9a233828286ebec728ad0cbcbe0a3caf6e90648e
Heron Oliveira <heron.hhoh@gmail.com>
2023-12-04 15:22:07 -0300

    04.12

--- raspagem/testes-invest2.py
+++ raspagem/testes-invest2.py
@@ -3,26 +3,37 @@ from bs4 import BeautifulSoup
 import pandas as pd
 #import time
 
-# - pesquisa 1 (Sobre os indicadores de crescimento CAGR):
+empresas = pd.read_excel('empresas-info-testes.xlsx')
+empresas_tickers = empresas['symbol'].str.strip()
+print(empresas_tickers)
 
-tickers = ['ddnb34', 'dxco3', 'ddnb34']
+tickers = empresas_tickers
+acoes_ou_bdrs = []
 
 for ticker in tickers:
-    print(ticker[-2:])
+    
+    # 1 - faz requisição
+    link = "https://statusinvest.com.br/acoes/"+ticker # VARIA: "bdrs" e "acoes"
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
+    requisicao = requests.get(link, headers=headers)
+    site = BeautifulSoup(requisicao.text, "html.parser")
+    print(requisicao)
 
-    if ticker[-2] != '34':
-        # 1 - se terminar em 34, fazer requisição para link de bdrs
-        link = "https://statusinvest.com.br/acoes/"+ticker # VARIA: "bdrs" e "acoes"
+    if (requisicao.status_code != 200):
+        acoes_ou_bdrs.append('deve-ser-bdr')
+
+        # 1 - faz requisição para brs
+        link = "https://statusinvest.com.br/bdrs/"+ticker # VARIA: "bdrs" e "acoes"
         headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
         requisicao = requests.get(link, headers=headers)
         site = BeautifulSoup(requisicao.text, "html.parser")
         print(requisicao)
     else:
-        # 1 - se não terminar em 34, fazer requisição para link de acoes
-        ticker = # o lugar no site onde pega o ticker original.
-        link = "https://statusinvest.com.br/acoes/eua/"+ticker # VARIA: "bdrs" e "acoes"
-        headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36"}
-        requisicao = requests.get(link, headers=headers)
-        site = BeautifulSoup(requisicao.text, "html.parser")
-        print(requisicao)
+        print('deve-ser-acao')
+        acoes_ou_bdrs.append('acao')
+
+empresas['tipo-de-acao'] = pd.Series(acoes_ou_bdrs)
+empresas.to_excel('empresas_br_tipos-teste.xlsx')
+    
+
 
